e559847e659901a7f2afca6c5830ee92b48677b9
2naain <jdlozano31@ucatolica.edu.co>
2026-06-04 00:43:03 -0500

    artist img

--- models/artist.py
+++ models/artist.py
@@ -3,8 +3,8 @@ from sqlmodel import SQLModel, Field
 
 class ArtistBase(SQLModel):
     name: str | None = Field(default=None, min_length=2, max_length=100)
-    is_active: bool = Field(default=True)
     image_url: str | None = Field(default=None, max_length=500)
+    is_active: bool = Field(default=True)
 
 
 class ArtistID(ArtistBase, table=True):
