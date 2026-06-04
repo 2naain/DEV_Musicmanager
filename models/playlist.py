7a4aedf1de3f25564e502bd56698d5dfec9512b4
2naain <jdlozano31@ucatolica.edu.co>
2026-06-04 01:49:39 -0500

    ss

--- models/playlist.py
+++ models/playlist.py
@@ -5,6 +5,7 @@ class PlaylistBase(SQLModel):
     name: str | None = Field(default=None, min_length=2, max_length=100)
     description: str | None = Field(default=None, max_length=500)
     is_active: bool = Field(default=True)
+    image_url: str | None = Field(default=None, max_length=500)
 
 
 class PlaylistID(PlaylistBase, table=True):
