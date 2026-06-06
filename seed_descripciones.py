4e3936cdd9c3c9ce4b383062230975dfcbac4d7d
2naain <jdlozano31@ucatolica.edu.co>
2026-06-05 20:22:34 -0500

    seed descriptions

--- seed_descripciones.py
+++ seed_descripciones.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("'postgresql://neondb_owner:npg_jqNx96hatfHP@ep-holy-salad-ap6gq8ve-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'")
 engine = create_engine(DATABASE_URL)
 
 descripciones = {
